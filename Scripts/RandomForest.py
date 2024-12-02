171b0bb65d0cfd158489585fd18144b42c7ebc8a
razi raouf <mohamedraoufrazi@gmail.com>
2024-12-02 12:32:31 +0100

    enhacing the performance of the model by incrementing the number of n_estimators

--- Scripts/RandomForest.py
+++ Scripts/RandomForest.py
@@ -21,7 +21,7 @@ y = df[target_column]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Initialize RandomForest Classifier
-rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_classifier = RandomForestClassifier(n_estimators=200, random_state=42)
 
 # Train the classifier
 rf_classifier.fit(X_train, y_train)
